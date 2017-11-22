27b45f067fdba5861689ed46e228434eb7700a83
Berg93 <sebastian.bergdahl@gmail.com>
2017-11-22 16:18:24 +0100

    Add files via upload

---
+++ Klimatalliansprojektet-master/klimat/js/learn-jquery.js
@@ -0,0 +1,9 @@
+//$ is the same as writing jQuery
+$(document).ready(function(){
+	//BASIC SELECTORS
+	//Detta väljer alla paragrafer på webbsidan och ger
+	//dem en 4 pixlar tjock kant
+	
+	//Det är helt enkelt taggen på insidan av parantesen som säger
+	//som påverkas av
+});
