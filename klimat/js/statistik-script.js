bc2501ff091cf89136b8d5cad1c7488e0a1ec047
davidahl <david.ahl.axelsson@gmail.com>
2017-11-24 14:40:32 +0100

    Add files via upload

--- klimat/js/statistik-script.js
+++ klimat/js/statistik-script.js
@@ -1,44 +1,65 @@
 $(document).ready(function(){
-    var labels=[];
-    $("#content > form").each(function() {
-        $(this).children('input').each(function() {
-        });
-    });
-    var ctx = document.getElementById("myChart").getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-    		data: {
-            labels: ["2014", "2015", "2016"],
-            datasets: [{
-    						label:"Transport",
-                data: [500,500,400],
-                backgroundColor: "rgba(180,50,50,1)",
-                hoverBackgroundColor: "rgba(150,50,50,1)"
-            },{
-    						label:"Lokala processer",
-                data: [600,600,500],
-                backgroundColor: "rgba(50,180,50,1)",
-                hoverBackgroundColor: "rgba(50,150,50,1)"
-            },{
+	
+		var labels=[];
+		var yearfrom = $("#yearselectfrom option:selected").text();
+		var yearto = $("#yearselectto option:selected").text();
+		var cars = [];
+		var lokala = [];
+		var transporter = [];
+		var flygresor = [];
+		for (i = yearfrom; i <= yearto; i++) { 
+			cars.push(i);
+			var x = Math.floor((Math.random() * 1000));
+			lokala.push(x);
+			var x = Math.floor((Math.random() * 1000));
+			transporter.push(x);
+			var x = Math.floor((Math.random() * 1000));
+			flygresor.push(x);
+			//lokala.push($sql select from lokalaprocesser where companyname = companyname;) 
+			//transporter.push($sql select from lokalaprocesser where companyname = companyname;
+			//lokala.push($sql select from lokalaprocesser where companyname = companyname;)
+			}
 
-    						label:"Flygresor",
-                data: [600,700,1000],
-                backgroundColor: "rgba(50,50,180,1)",
-                hoverBackgroundColor: "rgba(50,50,150,1)"
-            }]
-        },
-        options: {
-    			responsive:true,
-            scales: {
-    			xAxes: [{
-    				barThickness:50,
-    				stacked:true
-    			}],
-                yAxes: [{
-    							stacked:true
-                }]
-            }
-        }
-    });
 
+		
+		var ctx = document.getElementById("myChart").getContext('2d');
+		
+			var myChart = new Chart(ctx, {	
+				type: 'bar',
+			
+				data: {
+				labels: cars,
+				datasets: [{
+								label:"Transport",
+					data: transporter, // data: transport,
+					backgroundColor: "rgba(180,50,50,1)",
+					hoverBackgroundColor: "rgba(150,50,50,1)"
+				},{
+								label:"Lokala processer",
+					data: lokala,//data:lokala,
+					backgroundColor: "rgba(50,180,50,1)",
+					hoverBackgroundColor: "rgba(50,150,50,1)"
+				},{
+
+								label:"Flygresor",
+					data: flygresor,//data:flygresor,
+					backgroundColor: "rgba(50,50,180,1)",
+					hoverBackgroundColor: "rgba(50,50,150,1)"
+				}]
+			},
+			options: {
+					responsive:true,
+				scales: {
+					xAxes: [{
+						barThickness:50,
+						stacked:true
+					}],
+					yAxes: [{
+						stacked:true
+					}]
+				}
+			}
+		});
+			
+	
 });
