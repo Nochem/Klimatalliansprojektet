2501a98e4abb434fa3a386384a5c0c2caafcf77d
AegirA <aegir.atlason@gmail.com>
2017-11-28 11:44:30 +0100

    Inloggningsfunktionalitet php

---
+++ klimat/html/session.php
@@ -0,0 +1,16 @@
+<?php
+   include('mysqli_connect.php');
+   session_start();
+   
+   $user_check = $_SESSION['login_user'];
+   
+   $ses_sql = mysqli_query($dbc,"select Name from Users where Name = '$user_check' ");
+   
+   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+   
+   $login_session = $row['username'];
+   
+   if(!isset($_SESSION['login_user'])){
+      header("location:login.php");
+   }
+?>
