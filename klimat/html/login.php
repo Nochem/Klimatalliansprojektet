c8a41255af1c25bb883b2054b9c6b5a6da2a8fa1
AegirA <aegir.atlason@gmail.com>
2017-11-28 11:49:34 +0100

    Update login.php

--- klimat/html/login.php
+++ klimat/html/login.php
@@ -10,7 +10,7 @@
 		$count = mysqli_num_rows($result);
 		if($count == 1){
 $_SESSION['login_user'] = $myusername;
-header("location: welcome.php");
+header("location: rapport.html");
 			session_register("myusername"); //Får ej ligga över header
 			
 			
