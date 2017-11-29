6e4a64ce5c5b878cb72c6b3ae52aae2d0a3c5f2c
Christian <dat15csh@student.lu.se>
2017-11-29 21:16:24 +0100

    Kan skicka information till transport

--- klimat/php/testrapport.php
+++ klimat/php/testrapport.php
@@ -1,244 +1,181 @@
 <html>
 <head>
-<title>
-testrapport
-</title>
-<meta charset="UTF-8">
+    <title>
+        testrapport
+    </title>
+    <meta charset="UTF-8">
 </head>
 <body>
 
-<?php 
+<?php
 require_once('mysqli_connect.php');
 // Hämtar alla unika kategorier
 $categorySQL = "SELECT Distinct Category from ConversionFactors";
-
 // Skickar frågan till DB
-
 $response = @mysqli_query($dbc, $categorySQL);
 $emissionsqlresult = NULL;
 $lokalcount = 0;
 $transportcount = 0;
-
-
 // While rader finns i frågan
+while ($row = mysqli_fetch_assoc($response)) {
+    $category = $row['Category'];
 
-	while($row = mysqli_fetch_assoc($response)){
-		$category = $row['Category'];
-		
-		
-		
-		
-		
-		 if ($emissionsql = mysqli_prepare($dbc,"SELECT EmissionSource,Unit,convFactor,EmissionCO2perMWh from ConversionFactors where Category = ?")) {
-   $emissionsql->bind_param("s", $category);
-    /* execute query */
-    $emissionsql->execute();
-    /* instead of bind_result: */
-    $emissionsqlresult = $emissionsql->get_result();
-	/* now you can fetch the results into an array - NICE */
-		}
-		echo '<h1>';
-		echo $category ;
-		echo '</h1>';
-		echo  '<form action="#" method="get" name="form">';
-		echo '<table name= '.htmlspecialchars($category).' cellspacing="10">';
-		// Skapar rubriker till table
-			echo '<th> Utsläppskälla </th>';
-			echo '<th> Inköpt mängd</th>';
-			echo '<th> Mått </th>';
-			echo '<th> Omräknings Faktor </th>';
-			// echo '<th> Energi i MWh </th>';
-			echo '<th> Utsläpp CO2 per MWh </th>';
-			echo '<th> Ton CO2 </th>';
-			// echo '<th> Ton CO2 </th>';
-		while ($myrow = $emissionsqlresult->fetch_assoc()) {
-			if(!empty($myrow)){
-				
-			
-			
-			// Skapar innehåll i table
-				
-			echo '<tr name="row[]">';
-			if($category == "Lokaler och Processer"){
-				$lokalcount++;
-			}
-			if($category == "Transport"){
-				$transportcount++;
-			}
-			// Utsläppskälla
-			echo '<td >';
-			echo $myrow['EmissionSource'];
-			echo '</td>';
-			
-			 echo '<input type="hidden" name="emissionSource[]" value='.$myrow['EmissionSource'].'>';
-			
-			
-			// Mått name='.$myrow['EmissionSource'].'
-			echo '<td>';
-			echo '<input type="text" name= "amount[]"/>'; // onChange funktion behövs för att räkna ut enrgi i mwh
-			echo '</td>';
-			
-			// Enhet
-			echo '<td>';
-			echo '<select name="unit[]">';
-			echo '<option 
-	value =' .$myrow['Unit'] . '>' .$myrow['Unit'].
-	'</option>';
-	echo '</select>';
-	
-			echo '</td>';
-			// Omräkningsfaktor 
-			echo '<td id= convfactor[]>' .$myrow['convFactor']. '</td>';
-			 echo '<input type="hidden" name="convFactor[]" value='.$myrow['convFactor'].'>';
-			
-			// echo '<script>';
-			// echo energiFunction(){} 
-			// echo '</script>'
-			echo '<td id= >' .$myrow['EmissionCO2perMWh']. '</td>';
-			echo '<input type="hidden" name="emissionCO2[]" value='.$myrow['EmissionCO2perMWh'].'>';
-			// echo '<script>';
-			// echo tonCO2Function(){} 
-			// echo '</script>'
-			echo '<td>';
-			echo 0 ;
-			echo '</td>'; // behövs matt
-			echo '<input type="hidden" name="ton[]" value="0">';
-			
-			
-			echo '</tr>';
-						
-				
-					
-					
-					
-			}
-			
-		}
-		echo '</table>';
-		
-		
-		
-		
-		
-	}
-	
-	echo '<h1> Flygresor </h1>';
-	
-	echo '<table name ="Flygresor" cellspacing ="10">';
-		echo '<tr>';
-		echo '<th> Från </th>';
-		echo '<th> Till </th>';
-		echo '<th> Längd i km </th>';
-		echo '<th> KG CO2 </th>';
-		echo '</tr>';
-		echo '<td>';
-		echo '<input type="text" name="Från" >'; 
-		echo '</td>';
-		echo '<td>';
-		echo '<input type="text" name="Till" >'; 
-		echo '</td>';
-		echo '<td>';
-		echo '<input type="text" name="Längd i KM" >'; 
-		echo '</td>';
-		echo '<td>';
-		echo '<input type="text" name="KGCO2" >'; 
-		echo '</td>';
-		echo '</table>';
-		echo '<input type="submit" value = "spara" name ="Spara">';
-		
-		
-		echo '</form>';
-		
-		
-		
-		 if(isset($_GET['Spara'])){
-			 
-			 for($i = 0; $i <= 1; $i++){ 
-			 $emissionSource = $_GET['emissionSource'][$i];
-			// $amount = $_GET['amount'][$i];
-			 $unit = $_GET['unit'][$i];
-			 $convFactor = $_GET['convFactor'][$i];
-			 $emissionCO2 = $_GET['emissionCO2'][$i];
-			 $Ton = $_GET['ton'][$i];
-			 $id = "101";
-			/*  echo "<script type='text/javascript'>alert('$emissionSource');</script>";
-			  echo "<script type='text/javascript'>alert('$unit');</script>";
-			   echo "<script type='text/javascript'>alert('$convFactor');</script>";
-			    echo "<script type='text/javascript'>alert('$emissionCO2');</script>";
-				 echo "<script type='text/javascript'>alert('$Ton');</script>"; */
-				  
-			
-			 
-			 
-			 
-			
-			 
-			
-			
 
-					if ($insertTransportsql = mysqli_prepare($dbc,"INSERT INTO Transport (EmissionSource,Unit,ConvFactor,EmissionMwh,TonCO2,Id) values (?,?,?,?,?,?)")) {
-						echo "<script type='text/javascript'>alert('$Ton');</script>";
-						$insertTransportsql->bind_param("ssdddi", $emissionSource, $unit,$convFactor, $emissionCO2,$Ton,$id);
-						$insertTransportsql->execute();
-						$transportqlresult = $insertTransportsql->get_result();
-						$insertTransportsql->close();
+    if ($emissionsql = mysqli_prepare($dbc, "SELECT EmissionSource,Unit,convFactor,EmissionCO2perMWh from ConversionFactors where Category = ?")) {
+        $emissionsql->bind_param("s", $category);
+        /* execute query */
+        $emissionsql->execute();
+        /* instead of bind_result: */
+        $emissionsqlresult = $emissionsql->get_result();
+        /* now you can fetch the results into an array - NICE */
+    }
+    echo '<h1>';
+    echo $category;
+    echo '</h1>';
+    echo '<form action="#" method="get" name="form">';
+    echo '<table name= ' . htmlspecialchars($category) . ' cellspacing="10">';
+    // Skapar rubriker till table
+    echo '<th> Utsläppskälla </th>';
+    echo '<th> Inköpt mängd</th>';
+    echo '<th> Mått </th>';
+    echo '<th> Omräknings Faktor </th>';
+    // echo '<th> Energi i MWh </th>';
+    echo '<th> Utsläpp CO2 per MWh </th>';
+    echo '<th> Ton CO2 </th>';
+    // echo '<th> Ton CO2 </th>';
+    while ($myrow = $emissionsqlresult->fetch_assoc()) {
+        if (!empty($myrow)) {
 
-	
-				  
-			  
-			  
-			  				  			 
 
-			  
-			  
-			  
-		
-			
-			 }
-		 }
-		  
-		 }
-		
-				 
-				 
-				 
-				 
-				 
-		
- 
-		
-		
-			
-			
-			
-			
-			
-			
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		?>
+            // Skapar innehåll i table
 
-		
-	
-	
-	
-   
-		
-		
-		
-			
-	
-	
-	    
+            echo '<tr name="row[]">';
+            if ($category == "Lokaler och Processer") {
+                $lokalcount++;
+            }
+            if ($category == "Transport") {
+                $transportcount++;
+            }
+            // Utsläppskälla
+            echo '<td >';
+            echo $myrow['EmissionSource'];
+            echo '</td>';
+
+            echo '<input type="hidden" name="emissionSource[]" value=' . $myrow['EmissionSource'] . '>';
+
+
+            // Mått name='.$myrow['EmissionSource'].'
+            echo '<td>';
+            echo '<input type="text" name= "amount[]"/>'; // onChange funktion behövs för att räkna ut enrgi i mwh
+            echo '</td>';
+
+            // Enhet
+            echo '<td>';
+            echo '<select name="unit[]">';
+            echo '<option 
+	value =' . $myrow['Unit'] . '>' . $myrow['Unit'] .
+                '</option>';
+            echo '</select>';
+
+            echo '</td>';
+            // Omräkningsfaktor
+            echo '<td id= convfactor[]>' . $myrow['convFactor'] . '</td>';
+            echo '<input type="hidden" name="convFactor[]" value=' . $myrow['convFactor'] . '>';
+
+            // echo '<script>';
+            // echo energiFunction(){}
+            // echo '</script>'
+            echo '<td id= >' . $myrow['EmissionCO2perMWh'] . '</td>';
+            echo '<input type="hidden" name="emissionCO2[]" value=' . $myrow['EmissionCO2perMWh'] . '>';
+            // echo '<script>';
+            // echo tonCO2Function(){}
+            // echo '</script>'
+            echo '<td>';
+            echo 0;
+            echo '</td>'; // behövs matt
+            echo '<input type="hidden" name="ton[]" value="0">';
+
+
+            echo '</tr>';
+
+
+        }
+
+    }
+    echo '</table>';
+
+
+}
+
+echo '<h1> Flygresor </h1>';
+
+echo '<table name ="Flygresor" cellspacing ="10">';
+echo '<tr>';
+echo '<th> Från </th>';
+echo '<th> Till </th>';
+echo '<th> Längd i km </th>';
+echo '<th> KG CO2 </th>';
+echo '</tr>';
+echo '<td>';
+echo '<input type="text" name="Från" >';
+echo '</td>';
+echo '<td>';
+echo '<input type="text" name="Till" >';
+echo '</td>';
+echo '<td>';
+echo '<input type="text" name="Längd i KM" >';
+echo '</td>';
+echo '<td>';
+echo '<input type="text" name="KGCO2" >';
+echo '</td>';
+echo '</table>';
+echo '<input type="submit" value = "spara" name ="Spara">';
+
+
+echo '</form>';
+
+
+if (isset($_GET['Spara'])) {
+
+    for ($i = 0; $i <= 1; $i++) {
+        $emissionSource = $_GET['emissionSource'][$i];
+        // $amount = $_GET['amount'][$i];
+        $unit = $_GET['unit'][$i];
+        $convFactor = $_GET['convFactor'][$i];
+        $emissionCO2 = $_GET['emissionCO2'][$i];
+        $Ton = $_GET['ton'][$i];
+        $id = "101";
+        /*  echo "<script type='text/javascript'>alert('$emissionSource');</script>";
+          echo "<script type='text/javascript'>alert('$unit');</script>";
+           echo "<script type='text/javascript'>alert('$convFactor');</script>";
+            echo "<script type='text/javascript'>alert('$emissionCO2');</script>";
+             echo "<script type='text/javascript'>alert('$Ton');</script>"; */
+
+
+        if ($insertTransportsql = mysqli_prepare($dbc, "INSERT INTO Transport (EmissionSource,Unit,ConvFactor,EmissionMwh,TonCO2,Id) values (?,?,?,?,?,?)")) {
+            $insertTransportsql->bind_param("ssdddi", $emissionSource, $unit, $convFactor, $emissionCO2, $Ton, $id);
+            $insertTransportsql->execute();
+            $transportqlresult = $insertTransportsql->get_result();
+            $insertTransportsql->close();
+
+
+        }
+    }
+
+
+}
+echo '<form method="get" action="#">';
+echo '<input type="submit" name="delete" value="ta bort test data från databas" />';
+echo '</form>';
+
+if (isset($_GET['delete'])) {
+    $deleteSQL = "DELETE FROM Transport where Id = 101";
+    @mysqli_query($dbc, $deleteSQL);
+
+}
+
+
+?>
 
 
 </table>
