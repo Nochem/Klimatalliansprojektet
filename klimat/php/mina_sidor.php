4edb0b8aadf821e87b11a7f7001afb7420a8af0b
Berg93 <sebastian.bergdahl@gmail.com>
2017-11-29 22:16:36 +0100

    Update mina_sidor.php

--- klimat/php/mina_sidor.php
+++ klimat/php/mina_sidor.php
@@ -17,7 +17,8 @@
 <body>
 	<div id="user">
 		<p id="username">
-			User: <?php echo $row['Name']; ?>
+			Användare: <?php echo $row['Name']; ?>
+
 			<form style="float:right" id="logout" align="right" name="form1" method="post" action="statistik.php">
 				<label>
 					<input class="menuitem flatbutton" name="submit2" type="submit" id="submit2" value="Log out">
