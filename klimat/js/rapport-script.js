381b921c9b08e94f9cc2ef8fcf5b150cc33d2784
David Ahl <>
2017-11-30 11:00:01 +0100

    Save popup på rapportsidan
    ändringar i rapport.html, rapport.js och css.

--- klimat/js/rapport-script.js
+++ klimat/js/rapport-script.js
@@ -1,7 +1,59 @@
 $(document).ready(function(){
     
-	window.onbeforeunload = function(){
-		return "";
-	};
+	// window.onbeforeunload = function(){
+	// 	return "";
+	// };
+
+	// Get the modal
+	var modal = document.getElementById('myModal');
+
+	// Get the button that opens the modal
+	var btn = document.getElementById("myBtn");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks the button, open the modal 	
+	$(".modalSave").click(function() {
+		
+	    modal.style.display = "block";
+
+	})
+	// When the user clicks on <span> (x), close the modal
+	$(".close").click(function() {
+	    modal.style.display = "none";
+	   // modal.attr("display","none");
+	})
+	$("#save").click(function() {
+	    modal.style.display = "none";
+	   // modal.attr("display","none");
+	})
+
+	// When the user clicks anywhere outside of sthe modal, close it
+	$("#window").click(function(event) {
+	    if (event.target == modal) {
+	        modal.style.display = "none";
+	    }
+	})
+	// kontrollerar så att året består av 4 tecken
+	$("#saveCheck").click(function(){
+			var year = $("#modalInputYear");
+			var reportName = $("#modalInputReportName");
+			var name = $("#modalInputName");
+			
+			if(year.val()<2050 && year.val()>1999){
+				confirm("Rapport sparad");
+				
+			return true;
+			}else{
+				confirm("kontrollera år");
+			return false;
+
+			}
+
+
+		})
+
+
 
 });
