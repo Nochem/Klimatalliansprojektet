0623268d61b58f47c2293a930fee69196ea3c512
Christian <dat15csh@student.lu.se>
2017-12-06 11:02:36 +0100

    "Fungerande " historik sida

--- klimat/php/historik.php
+++ klimat/php/historik.php
@@ -1,21 +1,26 @@
 <?php
    include('session.php');
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html>
-<head>
-	<meta charset="UTF-8">
-	<title>
-		Klimat allians Lund - Historik
-	</title>
-	<link rel="stylesheet" type="text/css" href="../css/style-proto.css">
-	<link href="https://fonts.googleapis.com/css?family=Barlow" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="../css/historik-style.css">
-	<link rel="icon" href="../res/icon.png">
-</head>
-<body>
-	<div id="user">
+	<head>
+		<meta charset="UTF-8">
+		<title>
+			Klimat allians Lund - Historik
+		</title>
+		<link rel="stylesheet" type="text/css" href="../css/style-proto.css">
+		<link href="https://fonts.googleapis.com/css?family=Barlow" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="../css/historik-style.css">
+		<link rel="icon" href="../res/icon.png">
+	</head>
+	<body>
+
+		<div id="user">
 			<p id="username">
-				User: <?php echo $row['Name']; ?>
+			
+				User: <?php 
+				echo $login_session;
+					?>
 
 				<form style="float:right" id="logout" align="right" name="form1" method="post" action="statistik.php">
 					<label>
@@ -24,12 +29,12 @@
 				</form>
 			</p>
 	</div>
-	<div id="wrapper">
-		<a href="rapport.php"></a>
-		<div id="logo">
-		</div>
-		<div id="menu">
-			<ul>
+		<div id="wrapper">
+			<a href="rapport.html"></a>
+			<div id="logo">
+			</div>
+			<div id="menu">
+				<ul>
 				<a href="rapport.php">
 					<li class="menuitem" >
 						Rapport
@@ -57,37 +62,297 @@
 				</a>
 
 			</ul>
-		</div>
-		<div id="sidebar">
-		</div>
-		<div id="content">
-			<div id="stat">
-				<h1>
-					Inventering av CO<sub>2</sub> utsläpp från transporter
-				</h1>
-				<select id="yeardrop" onselect= "myfunction()">
-					<option>
-						2016
-					</option>
-					<option>
-						1337
-					</option>
-				</select>
-				<table>
-					<tr>
-						<th>Utsläppskälla</th>
-						<th>Inköpt mängd</th>
-						<th> Enhet</th>
-						<th> ton CO<sub>2</sub></th>
-					</tr>
-					<tbody id = "gentable">
-					</tbody>
-
-				</div>
 			</div>
-		</div>
+			<div id="sidebar">
+			</div>
+			<div id="content">
+				<div id="stat">
+					
+					
+					<form action="#" method="get" name="histDrop">
+					
+<?php
+ if ($yearSQL = mysqli_prepare($dbc, "SELECT Year from Report where User = ?")) {
+        $yearSQL->bind_param("s", $login_session);
+        /* execute query */
+        $yearSQL->execute();
+        /* instead of bind_result: */
+        $yearSQLresult = $yearSQL->get_result();
+        /* now you can fetch the results into an array - NICE */
+    }
+if(isset($yearSQLresult)){
+echo '<select id="yeardrop" name="yeardrop" onchange= "this.form.submit()">';
+echo '&nbsp';
+echo "<option value = 'väljettår'> Välj ett år </option>";
+	
+// mysqli_fetch_array returnerar en rad av data från queryn och fortsätter tills ingen mer data är tillgänglig
+while( $myrow = $yearSQLresult->fetch_assoc()){
+echo '<option 
+	value =' .$myrow['Year'] . '>' .$myrow['Year'].
+	'</option>';
+	 
+}
+echo '</select>';
+//echo '<input type="submit" name="submit" value="Välj" />';
+} else {
+	
+echo '<h1> Du har inga raporter <h1>';
+
+}
+?>
+
+
+
+</form>		
+			</div>
+			
+		
 		<script type="text/javascript" src="../js/jquery-3.2.1.slim.min.js"></script>
 		<script type="text/javascript" src="../js/proto-script.js"></script>
 		<script type="text/javascript" src="../js/historik-script.js"></script>
+		
+		
+	
+	 
+		 
+    <?php
+	if (isset($_GET['yeardrop'])){
+		
+		
+   $selectedYear = $_GET['yeardrop'];
+   
+   if($selectedYear != "väljettår"){
+   if ($LokalSql = mysqli_prepare($dbc, "SELECT EmissionSource,Round(TonCO2/(EmissionMwh*convFactor),2),Unit,Round(TonCO2/EmissionMwh,2),convFactor,EmissionMwh,TonCO2 FROM PlacesAndProcesses, Report where PlacesAndProcesses.Id = Report.Id AND YEAR(Report.Year) =?")) {
+   $LokalSql->bind_param("s", $selectedYear);
+    /* execute query */
+    $LokalSql->execute();
+    /* instead of bind_result: */
+    $PlacesRes = $LokalSql->get_result();
+    /* now you can fetch the results into an array - NICE */
+	
+   }else{
+	   
+   }
+   
+   
+   if ($OtherLokalSql = mysqli_prepare($dbc, "SELECT PlacesOwned,PlacesRentedOut,ProducedSolarElectricity,ProducedSolarHeat,Comment FROM OtherPlacesAndProcesses, Report where OtherPlacesAndProcesses.Id = Report.Id AND YEAR(Report.Year) = ?")) {
+   $OtherLokalSql->bind_param("s", $selectedYear);
+    /* execute query */
+    $OtherLokalSql->execute();
+    /* instead of bind_result: */
+    $OtherPlacesRes = $OtherLokalSql->get_result();
+    /* now you can fetch the results into an array - NICE */
+	
+   }else{
+	   
+   }
+   
+   if ($TransportSql = mysqli_prepare($dbc, "SELECT EmissionSource,Round(TonCO2/(EmissionMwh*convFactor),2),Unit,Round(TonCO2/EmissionMwh,2),TonCO2,convFactor,EnergyMwh,EmissionMwh FROM Transport, Report where Transport.Id = Report.Id AND YEAR(Report.Year) = ?")) {
+   $TransportSql->bind_param("s", $selectedYear);
+    /* execute query */
+    $TransportSql->execute();
+    /* instead of bind_result: */
+    $TransportRes = $TransportSql->get_result();
+    /* now you can fetch the results into an array - NICE */
+	
+   }else{
+	   
+   }
+   if ($OtherTransportSql = mysqli_prepare($dbc, "SELECT BioTransport,BioTransportAmount,EnforcementPurchasePolicyVehicle,EnforementTravelPolicy,EnviormentReqOtherTransport,EnviormentReqOtherTransportDescription,EnviormentReqPurchased,EnviormentReqPurchasedDescription,Comment FROM OtherTransport, Report where OtherTransport.Id = Report.Id AND YEAR(Report.Year) = ?")) {
+   $OtherTransportSql->bind_param("s", $selectedYear);
+    /* execute query */
+    $OtherTransportSql->execute();
+    /* instead of bind_result: */
+    $OtherTransportRes = $OtherTransportSql->get_result();
+    /* now you can fetch the results into an array - NICE */
+	
+   }else{
+	   
+   }
+   
+   if ($FlightSql = mysqli_prepare($dbc, "SELECT Departure,Destination,LengthKM,KGCO2 FROM Flights, Report where Flights.Id = Report.Id AND YEAR(Report.Year) =?")) {
+   $FlightSql->bind_param("s", $selectedYear);
+    /* execute query */
+    $FlightSql->execute();
+    /* instead of bind_result: */
+    $FlightRes = $FlightSql->get_result();
+    /* now you can fetch the results into an array - NICE */
+	
+   }else{
+	   
+	}}else{
+		
+	echo "På denna sida kan du se alla dina rapporter, välj ett år";
+		
+		
+	}}
+  
+?>
+	
+	
+	
+	
+	
+	<?php 
+	
+	if(isset($_GET['yeardrop'])){
+	
+	if($selectedYear != "väljettår"){
+		
+		echo '<h2 align= "center">  Lokaler och Proccesser </h2>'; 
+		
+		echo '<table align= "center">';
+		echo '<tr>';
+	echo '<th> Utsläppskälla </th>';
+	echo '<th> Mått </th>';
+	echo '<th> Enhet </th>';
+	echo '<th> Energi i MWh </th>';
+	echo '<th> Omräkningsfaktor </th>';
+	echo '<th> Utsläpp i Mwh </th>';
+	echo '<th> TonCO22 </th>';
+	echo '</tr>';
+	}
+		
+		
+	
+	
+	
+	
+	while ($myrow = $PlacesRes->fetch_assoc()) {
+	
+	
+	
+	if(!empty($myrow)){
+			
+		
+	echo '<tr>';
+    foreach($myrow as $field) {
+		if(empty($field)){
+		echo '<td align="center"> - </td>';
+		}else{
+        echo '<td align="center">' . htmlspecialchars($field) . '</td>';
+		}
+		
+    }
+	echo '</tr>';
+    
+        // use your $myrow array as you would with any other fetch
+       
+		}
+	}
+	echo '</table>';
+	
+	
+		
+	
+		
+		
+	
+	if($selectedYear != "väljettår"){
+	echo '<h3 align= "center"> Övrigt Lokaler och Proccesser </h3>'; 
+	echo '<table align= "center">';
+	echo '<tr>';
+	echo '<th> Lokaler som företaget äger (m2)  </th>';
+	echo '<th> Lokaler som hyrs ut </th>';
+	echo '<th> Produktion av solel </th>';
+	echo '<th> Produktion av solvärme </th>';
+	echo '<th> Kommentar </th>';
+	echo '</tr>';
+		
+		
+	}
+	
+	while ($myrow = $OtherPlacesRes->fetch_assoc()) {
+		if(!empty($myrow)){
+		
+			
+		
+		 echo '<tr>';
+    foreach($myrow as $field) {
+		if(empty($field)){
+		echo '<td align="center"> - </td>';
+		}else{
+        echo '<td align="center">' . htmlspecialchars($field) . '</td>';
+		}
+    }
+    echo '</tr>';
+        // use your $myrow array as you would with any other fetch
+       
+    }
+		
+	}
+	echo '</table>';
+	
+	
+	if($selectedYear != "väljettår"){
+	echo '<h2 align= "center"> Transport </h2>'; 
+	echo '<table align= "center">';
+	echo '<tr>';
+	echo '<th> Utsläppskälla </th>';
+	echo '<th> Utsläppskälla </th>';
+	echo '<th> Enhet </th>';
+	echo '<th> Energi i MWh </th>';
+	echo '<th> TonCO2 </th>';
+	echo '<th> Omräkningsfaktor </th>';
+	echo '<th> Energi i Mwh </th>';
+	echo '<th> Utsläpp i Mwh </th>';
+	echo '</tr>';
+	}
+	
+	while ($myrow = $TransportRes->fetch_assoc()) {
+		
+		if(!empty($myrow)){
+	
+			
+		
+		 echo '<tr>';
+    foreach($myrow as $field) {
+		if(empty($field)){
+		echo '<td align="center"> - </td>';
+		}else{
+        echo '<td align="center">' . htmlspecialchars($field) . '</td>';
+		}
+    }
+    echo '</tr>';
+        // use your $myrow array as you would with any other fetch
+      
+    }
+		
+	}
+	echo '</table>';
+	if($selectedYear != "väljettår"){	echo '<h2 align= "center"> Flygresor </h2>'; 
+			echo '<table align= "center">';
+	echo '<tr>';
+	echo '<th> Från </th>';
+	echo '<th> Till </th>';
+	echo '<th> Längd i KM</th>';
+	echo '<th> Kg CO2 </th>';
+	echo '</tr>';
+	}
+	
+	while ($myrow = $FlightRes->fetch_assoc()) {
+		if(!empty($myrow)){
+		
+			
+		
+		 echo '<tr>';
+    foreach($myrow as $field) {
+		if(empty($field)){
+		echo '<td align="center"> - </td>';
+		}else{
+        echo '<td align="center">' . htmlspecialchars($field) . '</td>';
+		}
+    }
+    echo '</tr>';
+        // use your $myrow array as you would with any other fetch
+       
+   }
+		
+	}
+	 echo '</table>';
+	}
+	?>
+	</div>
+	</div>
 	</body>
-	</html>
+</html>
