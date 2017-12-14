9f114a7686001461af7ece47131966a1cb99a872
Berg93 <sebastian.bergdahl@gmail.com>
2017-12-14 14:16:37 +0100

    Add files via upload

--- klimat/php/login.php
+++ klimat/php/login.php
@@ -13,27 +13,6 @@
 			$count = mysqli_num_rows($result);
 			if($count == 1){
 				$_SESSION['login_user'] = $myusername;
-				$date_and_time = date('Y-m-d H:i:s');
-				function get_client_ip() {
-    			$ipaddress = '';
-    			if (isset($_SERVER['HTTP_CLIENT_IP']))
-        		$ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-    			else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-        		$ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    			else if(isset($_SERVER['HTTP_X_FORWARDED']))
-        		$ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-    			else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
-        		$ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-    			else if(isset($_SERVER['HTTP_FORWARDED']))
-        		$ipaddress = $_SERVER['HTTP_FORWARDED'];
-    			else if(isset($_SERVER['REMOTE_ADDR']))
-        		$ipaddress = $_SERVER['REMOTE_ADDR'];
-    			else
-        		$ipaddress = 'UNKNOWN';
-    			return $ipaddress;
-				}
-				$ip=get_client_ip();
-				mysqli_query($dbc, "UPDATE Users SET LastLogIn='$date_and_time', IpAddress='$ip' WHERE Name='$myusername'");
 				if($myusername == "testadmin"){
 	 					header("Location: anvandare.php");
 	 			}else{
