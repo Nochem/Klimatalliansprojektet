ea00d1ce6f4e37bda0f5c19c9577805fb89feb9f
AegirA <aegir.atlason@gmail.com>
2017-12-15 10:44:34 +0100

    Fixat admin och varningsruta vid osparad rapport
    Fixat så att admin hamnar rätt vid inloggning och varningsrutan dyker upp när användare försöker lämna rapportsidan utan att ha sparat rapporten

---
+++ session.php
@@ -0,0 +1,16 @@
+<?php
+   include('mysqli_connect.php');
+   session_start();
+   
+   $user_check = $_SESSION['login_user'];
+   
+   $ses_sql = mysqli_query($dbc,"select Name, Admin, Password from Users where Name = '$user_check' ");
+   
+   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+   
+   $login_session = $row['Name'];
+   
+   if(!isset($_SESSION['login_user'])){
+      header("location:login.php");
+   }
+?>
